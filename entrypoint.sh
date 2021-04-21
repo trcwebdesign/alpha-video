a62c62b68c745489d31472e5a4e3b05dc85945ff
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-04-21 18:45:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "For support please visit the Github Project or send a message on our Disco
 echo "set nameserver to 1.1.1.1"
 echo nameserver 1.1.1.1 > /etc/resolv.conf
 echo "Starting Alpha-Video"
-python /app/alpha-video.py > /var/log/alpha-video.log&
+python /app/main.py > /var/log/alpha-video.log&
 echo "Starting Supervisord"
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 bash
