ddfded26f2d772599f973f50b7046a76e88fb3e1
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-05-03 11:50:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,12 @@ echo "set nameserver to 1.1.1.1"
 echo nameserver 1.1.1.1 > /etc/resolv.conf
 echo "Starting Alpha-Video"
 python /app/main.py > /var/log/alpha-video.log&
-echo "Starting Supervisord"
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+
+if [ -z "$localtunnel" ]
+then
+      echo "Starting Supervisord"
+      /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+else
+      echo "LocalTunnel disabled"
+fi
 bash
