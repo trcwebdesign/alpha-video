436a7c558167dc6fb67d276fb0ac0d820a1c8cd8
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-05-03 12:09:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ echo "/_/    \_\______|_|    |_|  |_/_/    \_\     \/   |_____|_____/|______\___
 echo "For support please visit the Github Project or send a message on our Discord server."
 echo "set nameserver to 1.1.1.1"
 echo nameserver 1.1.1.1 > /etc/resolv.conf
+echo "Starting Alpha-Video"
+python /app/main.py > /var/log/alpha-video.log&
 
 if [ -z "$localtunnel" ]
 then
@@ -18,5 +20,4 @@ else
       echo "LocalTunnel disabled"
 fi
 
-echo "Starting Alpha-Video"
-python /app/main.py > /var/log/alpha-video.log&
+sh
