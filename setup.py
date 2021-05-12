8d250a16b13087ccc456d55b45d10dee6f97bb7c
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-05-12 15:37:52 +0100

    fix dependences

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
 setup(
     name=PROJECT_PACKAGE_NAME,
-    version="1.45",
+    version="1.46",
     url=PROJECT_URL,
     project_urls=PROJECT_URLS,
     author=PROJECT_AUTHOR,
