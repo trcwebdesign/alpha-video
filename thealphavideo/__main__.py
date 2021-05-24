5c9ad1eb5f0e9d5dc6a71aa273b490913fb1a567
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-05-24 10:05:28 +0100

    Add session for variables

--- thealphavideo/__main__.py
+++ thealphavideo/__main__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, Response
 from pygtail import Pygtail
-from flask_ask_alphavideo import Ask, question, statement, convert_errors, audio
+from flask_ask_alphavideo import Ask, question, statement, convert_errors, audio, session
 from youtube_dl import YoutubeDL
 from werkzeug.exceptions import abort
 import sqlite3
@@ -195,7 +195,102 @@ def logstream():
 
 ask = Ask(app, '/api')
 
-import intents
+@ask.launch
+def launch():
+    card_title = render_template('card_title')
+    question_text = render_template('welcome')
+    return question(question_text).simple_card(card_title, question_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
+
+
+@ask.intent('AMAZON.StopIntent')
+def handle_stop_intent():
+    stop = render_template('stop')
+    return statement(stop)
+
+
+@ask.intent('AMAZON.CancelIntent')
+def handle_stop_intent():
+    stop = render_template('stop')
+    return statement(stop)
+
+
+@ask.intent('AMAZON.PauseIntent')
+def handle_pause_intent():
+    pause = render_template('pause')
+    return audio(pause).stop()
+
+
+@ask.intent('AMAZON.ResumeIntent')
+def resume():
+    resume = render_template('resume')
+    return audio(resume).resume()
+
+
+@ask.intent('AMAZON.FallbackIntent')
+def handle_fallback_intent():
+    fallback = render_template('fallback')
+    return question(fallback)
+
+
+@ask.intent('AMAZON.HelpIntent')
+def handle_help_intent():
+    fallback = render_template('fallback')
+    return question(fallback)
+
+
+@ask.intent('QueryIntent', mapping={'query': 'Query'})
+def handle_query_intent(query):
+
+    if not query or 'query' in convert_errors:
+	session.attributes[search_KEY] = query
+        return question('no results found, try another search query')
+
+    data = ytdl.extract_info(f"ytsearch:{query}", download=False)
+    search_results = data['entries']
+
+    if not search_results:
+        noresult = render_template('noresult')
+        return question(noresult)
+
+    result = search_results[0]
+    song_name = result['title']
+    channel_name = result['uploader']
+
+    for format_ in result['formats']:
+        if format_['ext'] == 'm4a':
+            mp3_url = format_['url']
+            playing = render_template('playing', song_name=song_name, channel_name=channel_name)
+            return audio(playing).play(mp3_url)
+
+    return question('noresult')
+
+@ask.on_playback_finished()
+def play_back_finished():
+    query = session.attributes.get(search_KEY)
+    
+    data = ytdl.extract_info(f"ytsearch:{query}", download=False)
+    search_results = data['entries']
+
+    if not search_results:
+        noresult = render_template('noresult')
+        return question(noresult)
+	
+    result = search_results[1]
+    song_name = result['title']
+    channel_name = result['uploader']
+
+    for format_ in result['formats']:
+        if format_['ext'] == 'm4a':
+            mp3_url = format_['url']
+            playing = render_template('playing', song_name=song_name, channel_name=channel_name)
+            return audio(playing).play(mp3_url)
+    
+    return question('noresult')
 
 app.run(host=host, port=port)
 
